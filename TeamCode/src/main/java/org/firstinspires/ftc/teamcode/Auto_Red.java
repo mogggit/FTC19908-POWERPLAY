1c96d06e2d46342954707807708e86b9177495b1
mogggit <moggit2023@gmail.com>
2023-01-13 18:59:52 +0800

    Lowered tfod min confidence

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Red.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Red.java
@@ -177,7 +177,7 @@ public class Auto_Red extends LinearOpMode {
         int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
                 "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
-        tfodParameters.minResultConfidence = 0.5f;
+        tfodParameters.minResultConfidence = 0.7f;
         tfodParameters.isModelTensorFlow2 = true;
         tfodParameters.inputSize = 300;
         tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
